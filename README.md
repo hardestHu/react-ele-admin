6b06ca006c5c0a009cc0bfb2e70b04d970825e4d
1427871171@qq.com <1427871171@qq.com>
2021-01-04 17:33:08 +0800

    添加备注

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-试着用react elementui 搭建后台管理系统
+试着用react elementui 搭建后台管理系统  
 2021-01-04 建立项目，添加路由
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route,Redirect} from 'react-router-dom'
 import Layout from '../components/layout'
 import Login from '../components/login'
-
+// <Redirect to="/layout/home"></Redirect>
 function AppRouter(){
   return (
     <Router>
       <Route path="/login" component={Login}></Route>
-      <Route path="/layout"  component={Layout}></Route>
-      <Route path="/" exact  component={Login}></Route>
+      <Route path="/layout" component={Layout}></Route>
+      <Redirect from="/" exact to="/login"></Redirect>
     </Router>
   )
 }
